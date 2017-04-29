5c004c5811861e98479af13efef7211ba8714981
melissaLam <lamxx204@umn.edu>
2017-04-28 19:53:25 -0500

    adding gitignore

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ res.sendFile(__dirname + '/server/static/index.html')
 
 
 // start the server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 });
